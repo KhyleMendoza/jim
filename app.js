f0bc9fe38ddf44fd3567bdad2a4d57e4f1a2878d
KhyleMendoza <127314673+KhyleMendoza@users.noreply.github.com>
2023-09-04 05:25:26 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -20,149 +20,119 @@ let isLoginMode = true;
 
 // Function to toggle between login and registration modes
 function toggleRegistration() {
-    isLoginMode = !isLoginMode;
-  
-    const emailField = document.getElementById('email');
-    const passwordField = document.getElementById('password');
-    const fullNameField = document.getElementById('full_name');
-    const loginButton = document.getElementById('loginButton');
-    const registerButton = document.getElementById('registerButton');
-    const toggleMessage = document.getElementById('toggleMessage'); // Get the toggle message element
-  
-    if (isLoginMode) {
-      fullNameField.style.display = 'none';
-      registerButton.style.display = 'none';
-      loginButton.style.display = 'block';
-      toggleMessage.textContent = "Not yet signed up? Register now"; // Change the text for login mode
-    } else {
-      fullNameField.style.display = 'block';
-      loginButton.style.display = 'none';
-      registerButton.style.display = 'block';
-      toggleMessage.textContent = "Already have an account? Login here"; // Change the text for registration mode
-    }
+  isLoginMode = !isLoginMode;
+
+  const emailField = document.getElementById('email');
+  const passwordField = document.getElementById('password');
+  const fullNameField = document.getElementById('full_name');
+  const loginButton = document.getElementById('loginButton');
+  const registerButton = document.getElementById('registerButton');
+  const toggleMessage = document.getElementById('toggleMessage');
+
+  if (isLoginMode) {
+    fullNameField.style.display = 'none';
+    registerButton.style.display = 'none';
+    loginButton.style.display = 'block';
+    toggleMessage.textContent = "Not yet signed up? Register now";
+  } else {
+    fullNameField.style.display = 'block';
+    loginButton.style.display = 'none';
+    registerButton.style.display = 'block';
+    toggleMessage.textContent = "Already have an account? Login here";
   }
-  
+}
+
 // Set up our register function
 function register() {
-  // Get all our input fields
-  email = document.getElementById('email').value;
-  password = document.getElementById('password').value;
-  full_name = document.getElementById('full_name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const full_name = document.getElementById('full_name').value;
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is Outta Line!!');
+  if (!validate_email(email) || !validate_password(password)) {
+    alert('Email or Password is invalid.');
     return;
-    // Don't continue running the code
   }
+
   if (!isLoginMode && !validate_field(full_name)) {
-    alert('Full Name is Outta Line!!');
+    alert('Full Name is invalid.');
     return;
   }
 
-  // Move on with Auth
   auth.createUserWithEmailAndPassword(email, password)
     .then(function () {
-      // Declare user variable
-      var user = auth.currentUser;
+      const user = auth.currentUser;
 
-      // Add this user to Firebase Database
-      var database_ref = database.ref();
-
-      // Create User data
-      var user_data = {
+      const user_data = {
         email: email,
         full_name: full_name,
         last_login: Date.now()
       };
 
-      // Push to Firebase Database
-      database_ref.child('users/' + user.uid).set(user_data);
+      const database_ref = database.ref();
 
-      // Redirect to home.html after successful registration
-      window.location.href = 'home.html';
+      database_ref.child('users/' + user.uid).set(user_data)
+        .then(function () {
+          console.log("User registered and data saved to the database.");
+          window.location.href = 'home.html';
+        })
+        .catch(function (error) {
+          console.error("Error saving user data to the database:", error);
+          alert("Error occurred while saving user data.");
+        });
     })
     .catch(function (error) {
-      // Firebase will use this to alert of its errors
-      var error_code = error.code;
-      var error_message = error.message;
-
-      alert(error_message);
+      console.error("Error registering user:", error);
+      alert("Error occurred while registering user.");
     });
 }
 
 // Set up our login function
 function login() {
-  // Get all our input fields
-  email = document.getElementById('email').value;
-  password = document.getElementById('password').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is Outta Line!!');
+  if (!validate_email(email) || !validate_password(password)) {
+    alert('Email or Password is invalid.');
     return;
-    // Don't continue running the code
   }
 
   auth.signInWithEmailAndPassword(email, password)
     .then(function () {
-      // Declare user variable
-      var user = auth.currentUser;
-
-      // Add this user to Firebase Database
-      var database_ref = database.ref();
-
-      // Create User data
-      var user_data = {
+      const user = auth.currentUser;
+      const user_data = {
         last_login: Date.now()
       };
 
-      // Push to Firebase Database
-      database_ref.child('users/' + user.uid).update(user_data);
+      const database_ref = database.ref();
 
-      // Redirect to home.html after successful login
-      window.location.href = 'home.html';
+      database_ref.child('users/' + user.uid).update(user_data)
+        .then(function () {
+          console.log("User logged in and last login time updated in the database.");
+          window.location.href = 'home.html';
+        })
+        .catch(function (error) {
+          console.error("Error updating last login time in the database:", error);
+          alert("Error occurred while updating last login time.");
+        });
     })
     .catch(function (error) {
-      // Firebase will use this to alert of its errors
-      var error_code = error.code;
-      var error_message = error.message;
-
-      alert(error_message);
+      console.error("Error logging in:", error);
+      alert("Error occurred while logging in.");
     });
 }
 
 // Validate Functions
 function validate_email(email) {
-  expression = /^[^@]+@\w+(\.\w+)+\w$/;
-  if (expression.test(email) == true) {
-    // Email is good
-    return true;
-  } else {
-    // Email is not good
-    return false;
-  }
+  const expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  return expression.test(email);
 }
 
 function validate_password(password) {
-  // Firebase only accepts lengths greater than 6
-  if (password.length < 6) {
-    return false;
-  } else {
-    return true;
-  }
+  return password.length >= 6;
 }
 
 function validate_field(field) {
-  if (field == null) {
-    return false;
-  }
-
-  if (field.length <= 0) {
-    return false;
-  } else {
-    return true;
-  }
+  return field != null && field.length > 0;
 }
 
 // Initialize the page with the login mode (full name hidden)
